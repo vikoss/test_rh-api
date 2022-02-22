cd81ccfda4b137e0a23971cb556a9cb190b4eec6
vikoss <vikoss.yiss@gmail.com>
2022-02-21 18:04:13 -0600

    add method to create employees

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -2,8 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Employee;
 use Illuminate\Http\Request;
+use App\Http\Requests\EmployeeRequest;
+
 class EmployeeController extends Controller
 {
-    //
+    public function store(EmployeeRequest $employee)
+    {
+        return Employee::create($employee->validated());
+    }
 }

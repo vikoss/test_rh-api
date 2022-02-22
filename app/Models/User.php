83c02e0dd8088791ef02bb39eaba1f7c652ef564
vikoss <vikoss.yiss@gmail.com>
2022-02-22 00:49:42 -0600

    add mutator for hash password

--- app/Models/User.php
+++ app/Models/User.php
@@ -63,4 +63,9 @@ class User extends Authenticatable implements JWTSubject
     {
         return [];
     }
+
+    public function setPasswordAttribute($password)
+    {
+        $this->attributes['password'] = bcrypt($password);
+    }
 }

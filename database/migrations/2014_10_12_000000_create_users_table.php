f31758596342a845e29fd7d09b8605be822acbdc
vikoss <vikoss.yiss@gmail.com>
2022-02-22 00:48:57 -0600

    add column role on users model

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('surname');
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('role')->default('employee');
             $table->timestamps();
         });
     }

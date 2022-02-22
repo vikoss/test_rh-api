b9de8b67144a767fd70968fea58626d35425c5ac
vikoss <vikoss.yiss@gmail.com>
2022-02-21 18:05:37 -0600

    add method to create jobs

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\JobRequest;
+use App\Models\Job;
 use Illuminate\Http\Request;
 
 class JobController extends Controller
 {
-    //
+    public function store(JobRequest $job)
+    {
+        return Job::create($job->validated());
+    }
 }

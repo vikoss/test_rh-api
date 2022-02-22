c06fbe0cce1d679601236db602178dd1319d475a
vikoss <vikoss.yiss@gmail.com>
2022-02-21 18:39:07 -0600

    add method to attach job to employees

--- app/Http/Controllers/EmployeeJobController.php
+++ app/Http/Controllers/EmployeeJobController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Employee;
 use Illuminate\Http\Request;
 
 class EmployeeJobController extends Controller
 {
-    //
+    public function store(Employee $employee, Request $request)
+    {
+        return $employee->jobs()->attach($request->jobId);
+    }
 }

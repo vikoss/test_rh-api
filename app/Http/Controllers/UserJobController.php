5771b0524633fb71b73eb27387d592989287ca4f
vikoss <vikoss.yiss@gmail.com>
2022-02-22 19:48:54 -0600

    add method to get all jobs of user

--- app/Http/Controllers/UserJobController.php
+++ app/Http/Controllers/UserJobController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserJobController extends Controller
 {
-    //
+    public function index(User $user)
+    {
+        return $user->employee()->first()->jobs()->get();
+    }
 }
